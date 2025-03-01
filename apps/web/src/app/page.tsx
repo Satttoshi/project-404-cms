d0532029bfb4136ffca5a34e0ec4e3c0752fc2e2
Joshua Stolle <joshuastolle445@gmail.com>
2025-03-01 02:47:47 +0100

    build: fix build error

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@repo/ui/button";
 export default function Home(): React.ReactElement {
   return (
     <>
-      <div className="text-4xl text-red bg-secondary">LOL</div>{" "}
+      <div className="text-4xl text-red bg-secondary">LOL</div>
       <Button appName="web">Open alert</Button>
     </>
   );
