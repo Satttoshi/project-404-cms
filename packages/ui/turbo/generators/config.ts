9e003adcc87bbfa6396dcdadd46092c39135ead1
Joshua Stolle <joshuastolle445@gmail.com>
2025-03-02 00:38:37 +0100

    feat: plop generator to generate in the index.ts instead of package.json

--- packages/ui/turbo/generators/config.ts
+++ packages/ui/turbo/generators/config.ts
@@ -1,51 +1,49 @@
-import type { PlopTypes } from "@turbo/gen";
+import type { PlopTypes } from '@turbo/gen';
 
 // Learn more about Turborepo Generators at https://turbo.build/repo/docs/core-concepts/monorepos/code-generation
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
   // A generator to add a new React component with proper folder structure
-  plop.setGenerator("react-component", {
-    description: "Adds a new React component with tests and stories",
+  plop.setGenerator('react-component', {
+    description: 'Adds a new React component with tests and stories',
     prompts: [
       {
-        type: "input",
-        name: "name",
-        message: "What is the name of the component?",
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the component?',
       },
     ],
     actions: [
       // Create the main component file
       {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
-        templateFile: "templates/component.hbs",
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/component.hbs',
       },
       // Create a test file
       {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx",
-        templateFile: "templates/component.test.hbs",
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+        templateFile: 'templates/component.test.hbs',
       },
       // Create a story file
       {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx",
-        templateFile: "templates/component.stories.hbs",
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/component.stories.hbs',
       },
       // Create an index file for clean exports
       {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/index.ts",
-        templateFile: "templates/component.index.hbs",
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/index.ts',
+        templateFile: 'templates/component.index.hbs',
       },
-      // Update package.json exports
+      // Update index.ts with new component export
       {
-        type: "append",
-        path: "package.json",
-        // @ts-ignore
-        pattern: /"exports": {(?<insertion>)/g,
-        template:
-          '    "./{{pascalCase name}}": "./src/components/{{pascalCase name}}/index.ts",',
+        type: 'modify',
+        path: 'index.ts',
+        pattern: /([\r\n]*)$/,
+        template: "\nexport * from './src/components/{{pascalCase name}}';\n",
       },
     ],
   });
